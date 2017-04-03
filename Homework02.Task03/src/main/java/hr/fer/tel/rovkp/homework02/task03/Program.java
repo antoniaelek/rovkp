8e5e9aa76f487bd5deaaa5de1edf84b927422b19
Antonia Elek <antoniaelek@hotmail.com>
2017-04-04 01:10:56 +0200

    task 3 cont'd

---
+++ Homework02.Task03/src/main/java/hr/fer/tel/rovkp/homework02/task03/Program.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.fer.tel.rovkp.homework02.task03;
+
+import hr.fer.tel.rovkp.homework02.task01.TripTimesMapper;
+import hr.fer.tel.rovkp.homework02.task01.TripTimesReducer;
+import hr.fer.tel.rovkp.homework02.task01.TripTimesTuple;
+import hr.fer.tel.rovkp.homework02.task02.LocationsMapper;
+import hr.fer.tel.rovkp.homework02.task02.LocationsPartitioner;
+import hr.fer.tel.rovkp.homework02.task02.LocationsReducer;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+/**
+ *
+ * @author aelek
+ */
+public class Program {
+    
+    private static final String INTERMEDIATE_PATH = "intermediate";
+    
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: <jar> <input path> <output path>");
+            return;
+        }
+
+        Job job = Job.getInstance();
+        job.setJarByClass(Program.class);
+        job.setJobName("Locations");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(INTERMEDIATE_PATH));
+
+        job.setMapperClass(LocationsMapper.class);
+        job.setPartitionerClass(LocationsPartitioner.class);
+        job.setReducerClass(LocationsReducer.class);
+        job.setNumReduceTasks(6);
+
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
+        
+        int code = job.waitForCompletion(true) ? 0 : 1;
+        
+        if (code == 0) {
+            Job bJob = Job.getInstance();
+            bJob.setJarByClass(SequentialDriver.class);
+            bJob.setJobName("TripTimes");
+
+            FileInputFormat.addInputPath(bJob, new Path(INTERMEDIATE_PATH));
+            FileOutputFormat.setOutputPath(bJob, new Path(args[1]));
+
+            bJob.setMapperClass(TripTimesMapper.class);
+            bJob.setCombinerClass(TripTimesReducer.class);
+            bJob.setReducerClass(TripTimesReducer.class);
+
+            bJob.setOutputKeyClass(Text.class);
+            bJob.setOutputValueClass(TripTimesTuple.class);
+
+            bJob.waitForCompletion(true);
+
+            code = bJob.waitForCompletion(true) ? 0 : 1;
+        }
+    }
+}
---
+++ Homework02.Task03/src/main/java/hr/fer/tel/rovkp/homework02/task03/SequentialDriver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.fer.tel.rovkp.homework02.task03;
+
+/**
+ *
+ * @author aelek
+ */
+public class SequentialDriver {
+    
+}
