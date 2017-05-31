2ae7ae3fda0a9d8cc56539e2352d3ebc65fd76cf
Antonia Elek <antoniaelek@hotmail.com>
2017-05-31 22:02:00 +0200

    hw04 task01 renames

--- Homework04.Task01/src/main/java/hr/fer/tel/rovkp/homework04/task01/SensorscopeReading.java
+++ Homework04.Task01/src/main/java/hr/fer/tel/rovkp/homework04/task01/SensorscopeReading.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hr.fer.tel.rovkp.homework04.task01;
 
 import java.time.LocalDateTime;
 
-/**
- *
- * @author aelek
- */
 public class SensorscopeReading implements Comparable<SensorscopeReading> {
     private int stationID;
     private int year;
